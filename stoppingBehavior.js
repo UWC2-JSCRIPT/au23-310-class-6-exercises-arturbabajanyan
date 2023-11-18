ad732f103c1b1a9180e299724f2747db455adcfb
arturbabajanyan <arturbabajanyanv@gmail.com>
2023-11-17 20:09:48 -0500

    stopping behavior is ready

--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -6,5 +6,14 @@ document.getElementById('cat').addEventListener('click', () => {
 // When clicked, "More info" link should alert "Here's some info"
 // instead of going to a new webpage
 
+$('#more-info').on('click', function(e) {
+  alert("Here's some info");
+  e.preventDefault();
+})
+
 // When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
+$('#dog').on('click', (e) => {
+  alert('Bow wow!');
+  e.stopPropagation();
+})
