ef52562457ce5a724d2b22900afb496fcd8917f6
arturbabajanyan <arturbabajanyanv@gmail.com>
2023-11-19 23:57:08 -0500

    toDoList updated final version, fading effect added

--- jQueryToDoList.js
+++ jQueryToDoList.js
@@ -1,41 +1,39 @@
-let ulElement = $('.today-list');
+$( document ).ready(function() {
+    alert( "ready!" );
 
-ulElement.on(('click'), (e) => {
-	if (e.target.nodeName.toLowerCase() == 'span') {
-		e.target.parentNode.classList.toggle('done');
-	}
-	if (e.target.className == 'delete') {
-		e.target.parentElement.remove();
-	}
-})
+  let ulElement = $('.today-list');
 
-const addListItem = function(e) {
-  e.preventDefault();
-  let text = $('input').val();
+  ulElement.on("click", "a", function(e) {
+    $(e.target).parent().fadeOut(720, function () {
+      $(e.target).closest('li').remove();
+    });
+  });
+  ulElement.on("click", "span", e => $(e.target).closest('li').toggleClass('done'));
 
-  if (text != '') {
+  const addListItem = function(e) {
+    e.preventDefault();
+    const text = $('input').val();
 
-    const newLiEl = $('<li>');
-    const newAEl = $('<a>');
-    newAEl.addClass('delete');
-    newAEl.text('Delete');
-  
-    const newSpanItem = $('<span>');
-    newSpanItem.text(`${text}\n`);
-  
-    newLiEl.append(newSpanItem, newAEl);
-    ulElement.append(newLiEl);
-  
-    $('input').val('')
-    
-  }
-}
+    if (text != '') {
 
-const addItem = $('a.add-item');
-addItem.on("click", addListItem);
+      const newLiEl = $('<li>');
+      const newAEl = $('<a>');
+      newAEl.addClass('delete');
+      newAEl.text('Delete');
+      
+      const newSpan = $('<span>');
+      newSpan.text(`${text}\n`)
 
+      newLiEl.append(newSpan, newAEl);
 
+      ulElement.append(newLiEl);
 
-// $(document).ready(function() {
-//   alert('Hello')
-// })
+      $('input').val('')
+
+    }
+  };
+
+  const addItem = $('.add-item');
+  addItem.on('click', addListItem)
+
+});
